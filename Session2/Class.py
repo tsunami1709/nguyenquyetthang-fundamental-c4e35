8c3dcd74dc1380a4efce0549e9d70a2b77bfc62f
Tsunami <Thang@Nguyens-MacBook-Pro-2.local>
2019-06-17 22:16:25 +0700

    Bài trên lớp buổi 2

---
+++ Session2/Class.py
@@ -0,0 +1,99 @@
+# age = float(input("Nhap mot so bat kì:"))
+# if age < 10:
+#     print("baby")
+
+# age = 10
+# is_baby = age < 10
+# print(is_baby)
+
+# num = 101
+# is_odd = num % 2 == 1
+# print(is_odd)
+
+# text = 'xin chào'
+# text_is_not_empty = len(text) > 0
+# print(text_is_not_empty)
+
+# #nhập giá trị tuyệt đối của một số
+# a = float(input("Nhập 1 số bất kì:"))
+# if a < 0:
+#     print("Giá trị tuyệt đối của a là: ", -a)
+# else:
+#     print("Giá trị tuyệt đối của a là: ", a)
+
+
+# age = input("Nhập tuổi của bạn: ")
+# while age.isdigit() == False :
+#     age = input("Nhập lại tuổi của bạn: ")
+# age = int(age)
+# while age <= 0:
+#     age = input("Nhập lại tuổi của bạn: ")
+# age = int (age)
+# if 0 < age < 13:
+#     print("Bạn là \"Baby\"!")
+# elif 13 <= age <= 19:
+#     print("Bạn là \"Teenager\"!")
+# else:
+#     print("Bạn là \"Adult\"!")
+
+# Giải phương trình bậc 2 với a != 0
+
+# for i in [1, 2, 3]:
+#     print("Hi", i)
+
+# for k in range(10):
+#     for v in range(k):
+#         print(v, end="")
+#     print()
+
+# for k in range(10):
+#     for v in range(10):
+#         for x in range(10):
+#             print(k, v, x)
+
+# age = "123456";  # chi co cac ky so trong chuoi nay
+# print (age.isdigit())
+# print (age.isdigit() == True)
+
+# #Sinh và in ra 1 dãy từ 1 đến 100
+# for i in range(1, 101, 1):
+#     print(i)    
+
+# #Sinh và in ra 1 dãy từ 100 đến 105
+# for i in range(100, 106, 1):
+#     print(i)   
+
+# #Sinh và in ra 1 dãy từ 8 xuống 105
+# for i in range(8, -1, -1):
+#     print(i) 
+
+# #Thực hiện tính tổng 1 dãy từ 1 đến n, với n nhập bởi người dùng
+# n = int(input("Nhập 1 số tự nhiên: "))
+# S = 0
+# for i in range(n+1):
+#     S = S + i
+# print("Tổng các số từ 1 đến n là: ", S) 
+
+# n = int(input("Nhập 1 số tự nhiên: "))
+# for i in range(n):
+#     print(i, end=" ")
+# for i in range(n, 0, -1):
+#     print(i, end=" ")
+
+# n = int(input("Nhập 1 số tự nhiên từ 1 đến 100: "))
+# S = 0
+# for i in range(1,n+1):
+#     S = S + 1/i
+# print ("Tổng cần tìm là: ", S)
+
+# n = int(input("Nhập 1 số tự nhiên từ 1 đến 100: "))
+# S = 1
+# for i in range(1, n+1):
+#     X = 1 
+#     for j in range(1, i+1):
+#         X = X*j
+#     S = S + 1/X
+# print("Tổng cần tìm là: ", S)
+
+
+
